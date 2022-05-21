1fd6b2f2e227cf7a85c915c1e2675ace9d63adcf
tanfeng95 <tanfeng95@hotmail.com>
2022-05-21 11:53:10 +0800

    update on cart delete button

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -62,28 +62,49 @@ export default function Cart({
     setQuantityList(updateQuantityList);
     console.log(updateQuantityList);
   };
-
+  const handleDeleteItem = (position) => {
+    console.log('delete');
+    console.log(position);
+    const updateCartList = itemlist.map((item, index) => {
+      if (position === index) {
+        localStorage.removeItem(`book id${item.id}`);
+      }
+    });
+    const values = [];
+    const keys = Object.keys(localStorage);
+    // console.log(keys);
+    for (let i = 0; i < keys.length; i++) {
+      // console.log(JSON.parse(localStorage.getItem(keys[i])));
+      values.push(JSON.parse(localStorage.getItem(keys[i])));
+    }
+    console.log(keys);
+    if (keys.length === 0) {
+      setItemList([]);
+    } else {
+      setItemList(values);
+    }
+  };
   // show items in cart and total value
   const cartList = itemlist.map((items, index) => (
     <div className="d-flex justify-content-center cart-item">
-      <input type="checkbox" className="checkbox" id={items.id} checked={checkState[index]} onChange={() => handleOnChange(index)} />
+      <input type="checkbox" className="checkbox" checked={checkState[index]} onChange={() => handleOnChange(index)} />
       <div className="image-cart">
         <figure><img src={items.image} alt="Album" /></figure>
       </div>
 
       <h6>
-        {items.title}
+        {items?.title}
       </h6>
-      <p>
-        {' '}
-        price
-        {items.price}
-      </p>
-      {/* <p>
-        quanity
-        {' '}
-        {items.quanity}
-      </p> */}
+      <div>
+        <p>
+          {' '}
+          price
+          {items.price}
+        </p>
+        <button className="btn btn-square" type="button" onClick={() => handleDeleteItem(index)}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+        </button>
+      </div>
       <select className="select select-bordered" onChange={(event) => handleSelectValue(index, event.target.value)}>
         <option>1</option>
         <option>2</option>
@@ -95,13 +116,18 @@ export default function Cart({
         <option>8</option>
         <option>9</option>
       </select>
+
     </div>
   ));
-
   return (
     <div>
       <Navbar />
-      {cartList}
+      {cartList.length > 0 && (
+      <>
+        {cartList}
+      </>
+      ) }
+
       <div>
         <h2>order Summary</h2>
         <div>
